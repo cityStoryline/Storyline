21aedde3baf627126c6b863b4b2070698866304f
jasondou <jasondou@umich.edu>
2016-04-13 00:00:53 -0400

    index setup

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function SwapDivsWithClick(div1)
 {
   div2 = "";
-  if(div1 == "description1"){
-    div2 = "description2";
+  if(div1 == "path1"){
+    div2 = "tourguide1";
   }
-  else {
-    div2 = "description1";
+  else if (div1 == "path2") {
+    div2 = "tourguide2";
+  }
+  else if (div1 == "tourguide1"){
+    div2 = "path1";
+  }
+  else if (div1 == "tourguide2") {
+    div2 = "path2";
   }
 
   d1 = document.getElementById(div1);
   d2 = document.getElementById(div2);
-
+  
   d1.style.display = "block";
   d2.style.display = "none";
 
